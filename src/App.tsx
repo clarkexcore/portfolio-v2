11af7f8fdc6060517c3c8dd15c13cd7b449d7bb4
Alex Clarke <clarkexcore@gmail.com>
2025-03-23 23:07:28 -0400

    adding some styling

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,15 @@ import { MainContainer } from "./components/common/common.styled";
 
 function App() {
 	return (
-		<Box sx={{ backgroundColor: "#000", minHeight: "100vh", p: 3 }}>
-			<MainContainer>
-				<Box>
-					<Typography level="title-md">Hey y'all</Typography>
-					<Typography level="title-lg">I'm Alex Clarke</Typography>
-				</Box>
-			</MainContainer>
-		</Box>
-	);
+        <Box sx={{ backgroundColor: 'common.black', minHeight: '100vh', p: 3 }}>
+            <MainContainer>
+                <Box>
+                    <Typography level="title-md">Hey y'all</Typography>
+                    <Typography level="title-lg">I'm Alex Clarke</Typography>
+                </Box>
+            </MainContainer>
+        </Box>
+    );
 }
 
 export default App;
--- src/components/ThemeProvider.tsx
+++ src/components/ThemeProvider.tsx
@@ -1,22 +1,41 @@
-import { CssBaseline, CssVarsProvider, Sheet } from "@mui/joy";
+import { CssBaseline, CssVarsProvider, extendTheme, Sheet } from '@mui/joy';
 
 interface ThemeProviderProps {
-	children: React.ReactNode;
+    children: React.ReactNode;
 }
 
+const theme = extendTheme({
+    colorSchemes: {
+        dark: {
+            palette: {
+                common: {
+                    black: '#0E0E10',
+                },
+            },
+        },
+        light: {
+            palette: {
+                common: {
+                    black: '#0E0E10',
+                },
+            },
+        },
+    },
+});
+
 export function ThemeProvider({ children }: ThemeProviderProps) {
-	return (
-		<CssVarsProvider>
-			<CssBaseline />
-			<Sheet
-				sx={{
-					width: "100%",
-					minHeight: "100svh",
-					backgroundColor: theme => theme.palette.background.surface,
-				}}
-			>
-				{children}
-			</Sheet>
-		</CssVarsProvider>
-	);
+    return (
+        <CssVarsProvider theme={theme}>
+            <CssBaseline />
+            <Sheet
+                sx={{
+                    width: '100%',
+                    minHeight: '100svh',
+                    backgroundColor: theme => theme.palette.background.surface,
+                }}
+            >
+                {children}
+            </Sheet>
+        </CssVarsProvider>
+    );
 }
